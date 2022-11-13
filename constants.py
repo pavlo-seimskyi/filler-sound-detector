56b906aa6db2c2c8d17f36f49bf79b58f5aca27e
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2022-11-13 17:44:18 +0100

    feat: add label parser (#3)
    This module reads the long txt file generated by the software where the labels were created, and parses the start and end time of the filler sounds. It saves the parsed annotations in separate csv files for every speaker.

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
+import os
+
 WIN_LENGTH = 2048
 HOP_LENGTH = WIN_LENGTH
 FILLER_LABELING_THRESHOLD = 0.8
+SAMPLE_RATE = 8000
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+PARSED_ANNOTATION_FOLDER = "data/labels/parsed_annotations"
